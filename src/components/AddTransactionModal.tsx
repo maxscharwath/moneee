b005eb0f9506eb52a89d4257ad6b9fe241470cb3
Maxime Scharwath <maxscharwath@gmail.com>
2023-10-23 23:51:51 +0200

    feat: add categories select

--- src/components/AddTransactionModal.tsx
+++ src/components/AddTransactionModal.tsx
@@ -1,11 +1,39 @@
-import React, {useMemo} from 'react';
+import React, { useMemo } from 'react'
 import {Button} from '@/components/ui/button.tsx';
 import * as TabsGroup from '@/components/ui/tabs-group.tsx';
-import {Calendar, Check, Delete, LayoutGrid} from 'lucide-react';
+import { Calendar, Check, Delete, LayoutGrid } from 'lucide-react'
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select.tsx'
+import { useCategoryService } from '@/stores/categoryService.tsx'
+
+const NumericButton = ({value, hasDecimal, appendToAmount}: {
+	value: string;
+	hasDecimal: boolean;
+	appendToAmount: (value: string) => void;
+}) => (
+	<Button
+		size='xl'
+		key={value}
+		onClick={() => {
+			appendToAmount(value);
+		}}
+		disabled={value === '.' && hasDecimal}
+	>
+		{value}
+	</Button>
+);
+
+const parseNumberFromString = (str: string): number | null => {
+	const normalizedString = str
+		.replace(/[^0-9.,']/g, '')
+		.replace(/'/g, '')
+		.replace(',', '.');
+	const matched = /(\d+(\.\d)?)/.exec(normalizedString);
+	return matched ? parseFloat(matched[0]) : null;
+};
 
 export const AddTransactionModal = () => {
 	const [amount, setAmount] = React.useState('0');
-
+	const {categories} = useCategoryService();
 	const hasDecimal = useMemo(() => amount.includes('.'), [amount]);
 	const decimalPlaces = useMemo(() => {
 		const parts = amount.split('.');
@@ -33,6 +61,14 @@ export const AddTransactionModal = () => {
 		setAmount(prev => prev.length > 1 ? prev.slice(0, -1) : '0');
 	};
 
+	const handlePaste = async () => {
+		const text = await navigator.clipboard.readText();
+		const number = parseNumberFromString(text);
+		if (number !== null) {
+			setAmount(number.toString());
+		}
+	}
+
 	const formatAmount = useMemo(() => {
 		const fractionDigits = hasDecimal ? 2 : 0;
 		return Number(amount).toLocaleString('fr-CH', {
@@ -42,19 +78,6 @@ export const AddTransactionModal = () => {
 			maximumFractionDigits: fractionDigits,
 		});
 	}, [amount, hasDecimal]);
-
-	const NumericButton = ({value}: {value: string}) => (
-		<Button
-			size='xl'
-			key={value}
-			onClick={() => {
-				appendToAmount(value);
-			}}
-			disabled={value === '.' && hasDecimal}
-		>
-			{value}
-		</Button>
-	);
 
 	const [value, setValue] = React.useState('expense');
 
@@ -78,8 +101,8 @@ export const AddTransactionModal = () => {
 				</TabsGroup.Root>
 			</nav>
 			<div className='flex-grow grid grid-cols-[1fr,auto,1fr] gap-4 items-center'>
-				<div></div>
-				<span className='text-center text-4xl font-extrabold truncate'>
+				<div/>
+				<span className='text-center text-4xl font-extrabold truncate' onClick={handlePaste}>
 					{formatAmount}
 				</span>
 				<div className='flex flex-col items-end'>
@@ -98,14 +121,27 @@ export const AddTransactionModal = () => {
 						dateStyle: 'medium',
 					})}
 				</Button>
-				<Button variant='outline'>
-					<LayoutGrid className='h-4 w-4 mr-2' />
-					Select category
-				</Button>
+				<Select>
+					<SelectTrigger>
+						<SelectValue
+							placeholder={(<div className='inline-flex items-center justify-center'><LayoutGrid className='h-4 w-4 mr-2' /> Select category</div>)}
+						/>
+					</SelectTrigger>
+					<SelectContent position='item-aligned'>
+						{categories.map(category => (
+							<SelectItem key={category.id} value={category.id}>
+								<div className='flex items-center space-x-2'>
+								<span>{category.icon}</span>
+								<span>{category.name}</span>
+								</div>
+							</SelectItem>
+						))}
+					</SelectContent>
+				</Select>
 			</div>
 			<div className='grid grid-cols-3 gap-4 max-w-lg w-full place-self-center'>
 				{['1', '2', '3', '4', '5', '6', '7', '8', '9', '.', '0'].map(value => (
-					<NumericButton key={value} value={value} />
+					<NumericButton key={value} value={value} hasDecimal={hasDecimal} appendToAmount={appendToAmount} />
 				))}
 				<Button disabled={!isValidAmount()} size='xl'>
 					<Check />
--- src/components/ui/select.tsx
+++ src/components/ui/select.tsx
@@ -17,7 +17,7 @@ React.ComponentPropsWithoutRef<typeof SelectPrimitive.Trigger>
 	<SelectPrimitive.Trigger
 		ref={ref}
 		className={cn(
-			'flex h-10 w-full items-center justify-between rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
+			'flex h-10 items-center justify-between rounded-md border border-input bg-background px-3 py-2 text-md ring-offset-background placeholder:text-muted-foreground focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
 			className,
 		)}
 		{...props}
@@ -66,7 +66,7 @@ React.ComponentPropsWithoutRef<typeof SelectPrimitive.Label>
 >(({className, ...props}, ref) => (
 	<SelectPrimitive.Label
 		ref={ref}
-		className={cn('py-1.5 pl-8 pr-2 text-sm font-semibold', className)}
+		className={cn('py-1.5 pl-8 pr-2 text-md font-semibold', className)}
 		{...props}
 	/>
 ));
@@ -79,7 +79,7 @@ React.ComponentPropsWithoutRef<typeof SelectPrimitive.Item>
 	<SelectPrimitive.Item
 		ref={ref}
 		className={cn(
-			'relative flex w-full cursor-default select-none items-center rounded-sm py-1.5 pl-8 pr-2 text-sm outline-none focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50',
+			'relative flex w-full cursor-default select-none items-center rounded-md py-1.5 pl-8 pr-2 text-md outline-none focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50',
 			className,
 		)}
 		{...props}
